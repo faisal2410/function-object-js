144902eb0c465161917d787a1fad631852c3e9fe
Faisal Ahmed <faisal2410@yahoo.com>
2022-09-07 10:04:33 +0600

    initial commit

---
+++ return.js
@@ -0,0 +1,34 @@
+function add1(number1,number2){
+    var sum1=number1+number2;
+    // console.log(sum1);
+    return sum1;
+    
+}
+var result=add1(2,3);
+console.log(result);
+
+// function add(number1, number2){
+//     console.log(number1, number2);
+//     var sum = number1 + number2;
+//     // console.log(sum);
+//     return sum;
+//     console.log('I need more code');
+//     return 15;
+//     return 'hello done';
+//     return 'I am hungry';
+// }
+
+// // add(45, 15);
+
+// var total = add(80, 20);
+// // console.log('total', total);
+
+// function bringSingara(money){
+//     var singaraPrice = 10;
+//     var quantity = money / singaraPrice;
+//     return quantity;
+// }
+
+// var myTaka = 150
+// var singaras = bringSingara(myTaka);
+// console.log('Eating singaras: ', singaras);
